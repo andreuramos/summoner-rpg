b905e6c8124f5ee287350583ec2016eb6b47327a
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2021-12-27 22:04:04 +0100

    player move

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -1,22 +1,29 @@
 extends KinematicBody2D
 
 const MAX_SPEED = 400
-const ACCELERATION = 50
-const FRICTION = 100
+const ACCELERATION = 800
+const FRICTION = 1200
 
 var velocity = Vector2.ZERO
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
-
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector.normalized() * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED * delta)
+		animationTree.set("parameters/idle/blend_position", input_vector)
+		animationTree.set("parameters/walk/blend_position", input_vector)
+		animationState.travel("walk")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		print(animationTree.get("parameters/idle/blend_position"))
+		animationState.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
 		
